055988994ecb6c88aff979b83cb100a71a3d281f
wongguo <1361820008@qq.com>
2022-08-22 16:49:36 +0800

    登陆注册部分UI

--- app/src/main/java/com/example/tododemo/account/LoginFragment.java
+++ app/src/main/java/com/example/tododemo/account/LoginFragment.java
@@ -9,13 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.tododemo.R;
-
 public class LoginFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_login,container,false);
-        return view;
+
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
