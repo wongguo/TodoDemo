ab00a720508d70a2db8184ee75f3c260578b54cd
86131 <577300715@qq.com>
2022-08-23 22:31:28 +0800

    顶部导航栏颜色调整

--- app/src/main/java/com/example/tododemo/account/RegisterFragment.java
+++ app/src/main/java/com/example/tododemo/account/RegisterFragment.java
@@ -15,7 +15,7 @@ public class RegisterFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_register,container,false);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
         return view;
     }
 }
