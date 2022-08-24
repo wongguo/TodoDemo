5f66623528dbd082cc08fbe3951d845768ed3505
wongguo <1361820008@qq.com>
2022-08-24 23:44:15 +0800

    微调UI+基本设置

---
+++ app/src/main/java/com/example/tododemo/SQLite/Constant.java
@@ -0,0 +1,8 @@
+package com.example.tododemo.SQLite;
+
+public class Constant {
+    public static final String DATABASE_NAME="userDatabase";
+    public static final int DATABASE_VERSION=1;
+    public static final String ACCOUNT_TABLE_NAME="userTable";
+    public static final String TODO_TABLE_NAME="todoTable";
+}
