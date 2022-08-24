3bc66693c1ff1066b948c16ec22eda3773e6e706
wongguo <1361820008@qq.com>
2022-08-24 16:48:18 +0800

    微调UI+基本设置

--- app/src/main/java/com/example/tododemo/account/AccountActivity.java
+++ app/src/main/java/com/example/tododemo/account/AccountActivity.java
@@ -5,12 +5,35 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.tododemo.BaseActivity;
 import com.example.tododemo.R;
+import com.google.android.material.appbar.MaterialToolbar;
 
-public class AccountActivity extends AppCompatActivity {
+public class AccountActivity extends BaseActivity {
+
+    private MaterialToolbar mtb_account;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account);
+
+
     }
+
+    @Override
+    protected int getLayoutRes() {
+        return R.layout.activity_account;
+    }
+
+    @Override
+    protected void initView() {
+        mtb_account = findViewById(R.id.mtb_account);
+    }
+
+    @Override
+    protected void initListener() {
+        mtb_account.setNavigationOnClickListener(view -> finish());
+    }
+
+
 }
--- app/src/main/java/com/example/tododemo/account/RegisterFragment.java
+++ app/src/main/java/com/example/tododemo/account/RegisterFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.example.tododemo.R;
 
@@ -16,6 +17,12 @@ public class RegisterFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_register, container, false);
+
+        //注册跳转登录
+        view.findViewById(R.id.registerToLogin)
+                .setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_registerFragment_to_loginFragment));
+
+
         return view;
     }
 }
