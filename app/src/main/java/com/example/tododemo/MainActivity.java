6907343ff371ef16b6db960f1b894b12e36598f9
wongguo <1361820008@qq.com>
2022-08-24 16:48:18 +0800

    微调UI+基本设置

--- app/src/main/java/com/example/tododemo/MainActivity.java
+++ app/src/main/java/com/example/tododemo/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.tododemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.tododemo.account.AccountActivity;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends BaseActivity {
+
+    private MaterialToolbar mtb_main;
+    private MaterialButton mb_search;
+    private FloatingActionButton fab_add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected int getLayoutRes() {
+        return R.layout.activity_main;
+    }
+
+    /**
+     * 初始化控件
+     */
+    @Override
+    protected void initView() {
+        mtb_main = findViewById(R.id.mtb_main);
+        mb_search = findViewById(R.id.mb_search);
+        fab_add = findViewById(R.id.fab_add);
+    }
+
+    /**
+     * 控件点击事件
+     */
+    @Override
+    protected void initListener() {
+        //用户登录界面跳转
+        mtb_main.setNavigationOnClickListener(view -> {
+            //如果未登录
+            Intent intent=new Intent(MainActivity.this, AccountActivity.class);
+            startActivity(intent);
+            //如果登录弹出退出登录dialog
+
+        });
+
+        //搜索功能
+        mb_search.setOnClickListener(view -> {
+
+        });
+
+        //添加todo
+        fab_add.setOnClickListener(view -> {
+
+        });
+    }
+
 }
