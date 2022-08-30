13570bc8881065761f7e8f98ce010b121d0a22c8
wongguo <1361820008@qq.com>
2022-08-30 15:44:30 +0800

    登陆成功后状态回调，本地自动登录

--- app/src/main/java/com/example/tododemo/MainActivity.java
+++ app/src/main/java/com/example/tododemo/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.tododemo;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.example.tododemo.SQLite.Constant;
+import com.example.tododemo.SQLite.UserDatabase;
 import com.example.tododemo.account.AccountActivity;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends BaseActivity {
@@ -13,6 +19,7 @@ public class MainActivity extends BaseActivity {
     private MaterialToolbar mtb_main;
     private MaterialButton mb_search;
     private FloatingActionButton fab_add;
+    private ExtendedFloatingActionButton efab_account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +39,33 @@ public class MainActivity extends BaseActivity {
         mtb_main = findViewById(R.id.mtb_main);
         mb_search = findViewById(R.id.mb_search);
         fab_add = findViewById(R.id.fab_add);
+        efab_account = findViewById(R.id.efab_account);
+        initSQL();
     }
+
+
+    //初始化数据库->自动登录
+    protected void initSQL(){
+        UserDatabase userDatabase=new UserDatabase(MainActivity.this);
+        SQLiteDatabase sqLiteDatabase=userDatabase.getWritableDatabase();
+        //遍历数据库
+        Cursor cursor=sqLiteDatabase.query(Constant.ACCOUNT_TABLE_NAME,null,null,null,null,null,null);
+        while (cursor.moveToNext()){
+            int index_user=cursor.getColumnIndex("username");
+            String exist_user=cursor.getString(index_user);
+            int index_isLogin=cursor.getColumnIndex("isLogin");
+            String exist_isLogin=cursor.getString(index_isLogin);
+            if("true".equals(exist_isLogin)){
+                Constant.username=exist_user;
+                Constant.isLogin=true;
+                efab_account.setText(exist_user);
+                cursor.close();
+                break;
+            }
+        }
+        cursor.close();
+    }
+
 
     /**
      * 控件点击事件
@@ -40,12 +73,16 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initListener() {
         //用户登录界面跳转
-        mtb_main.setNavigationOnClickListener(view -> {
-            //如果未登录
-            Intent intent=new Intent(MainActivity.this, AccountActivity.class);
-            startActivity(intent);
+        efab_account.setOnClickListener(view -> {
             //如果登录弹出退出登录dialog
+            if(Constant.isLogin){
 
+            }
+            //如果未登录
+            else {
+                Intent intent=new Intent(MainActivity.this, AccountActivity.class);
+                startActivity(intent);
+            }
         });
 
         //搜索功能
@@ -59,4 +96,9 @@ public class MainActivity extends BaseActivity {
         });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        efab_account.setText(Constant.username);
+    }
 }
