fd51126cd6b16e6acb02a64230674368184d6333
wongguo <1361820008@qq.com>
2022-09-05 21:16:31 +0800

    添加todo的MaterialDatePicker设置

---
+++ app/src/main/java/com/example/tododemo/todo/TodoAdapter.java
@@ -0,0 +1,4 @@
+package com.example.tododemo.todo;
+
+public class TodoAdapter {
+}
--- app/src/main/java/com/example/tododemo/todo/TodoDialog.java
+++ app/src/main/java/com/example/tododemo/todo/TodoDialog.java
@@ -1,5 +1,6 @@
 package com.example.tododemo.todo;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.widget.Toast;
 
@@ -8,6 +9,8 @@ import androidx.fragment.app.FragmentManager;
 
 import com.example.tododemo.Dialog.BaseDialog;
 import com.example.tododemo.R;
+import com.example.tododemo.SQLite.CRUD;
+import com.example.tododemo.SQLite.Constant;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
@@ -21,11 +24,12 @@ public class TodoDialog extends BaseDialog {
 
     private final Context context;
     private final FragmentManager fragmentManager;
+    private String select_time = null;
     private MaterialButton mb_add;
     private TextInputEditText ti_et_title;
     private TextInputEditText ti_et_classify;
     private TextInputEditText ti_et_time;
-    private TextInputLayout til_title;
+    private MaterialDatePicker<Long> picker;
 
     public TodoDialog(@NonNull Context context, FragmentManager fragmentManager) {
         super(context);
@@ -40,31 +44,40 @@ public class TodoDialog extends BaseDialog {
 
     @Override
     protected void initView() {
-        til_title = findViewById(R.id.til_title);
         ti_et_title = findViewById(R.id.ti_et_title);
         ti_et_classify = findViewById(R.id.ti_et_classify);
         ti_et_time = findViewById(R.id.ti_et_time);
         mb_add = findViewById(R.id.mb_add);
+        //初始化日期选择器
+        initDatePicker();
+    }
+
+    private void initDatePicker() {
+        MaterialDatePicker.Builder<Long> builder = MaterialDatePicker.Builder.datePicker();
+        //设置MaterialDatePicker初始选中今天
+        builder.setSelection(MaterialDatePicker.todayInUtcMilliseconds());
+        //设置MaterialDatePicker模式
+        builder.setInputMode(MaterialDatePicker.INPUT_MODE_CALENDAR);
+        //初始化日期选择器->选择今天
+        select_time=longToDate(MaterialDatePicker.todayInUtcMilliseconds());
+        ti_et_time.setText(select_time);
+        picker = builder.build();
     }
 
     @Override
     protected void initListener() {
         ti_et_time.setOnClickListener(view -> {
-            MaterialDatePicker.Builder<Long> builder=MaterialDatePicker.Builder.datePicker();
-            builder.setSelection(MaterialDatePicker.todayInUtcMilliseconds());
-            builder.setInputMode(MaterialDatePicker.INPUT_MODE_CALENDAR);
-            MaterialDatePicker<Long> picker=builder.build();
             picker.show(fragmentManager,picker.toString());
-            picker.addOnPositiveButtonClickListener(selection -> ti_et_time.setText(longToDate(selection)));
+            //监听日期选中后
+            picker.addOnPositiveButtonClickListener(selection -> {
+                select_time=longToDate(selection);
+                ti_et_time.setText(select_time);
+            });
         });
 
 
         mb_add.setOnClickListener(view -> {
-            String title=ti_et_title.getText().toString();
-            String classify=ti_et_classify.getText().toString();
-            if(title.isEmpty()){
-                Toast.makeText(context, "", Toast.LENGTH_SHORT).show();
-            }
+
         });
     }
 
